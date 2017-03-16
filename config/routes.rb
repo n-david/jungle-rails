a792a7875677b019a5340e8a6e5d83c75bea2bf3
n-david <ngdavid.93@gmail.com>
2017-03-16 16:59:45 +0000

    Removed comments

--- config/routes.rb
+++ config/routes.rb
@@ -12,15 +12,7 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
-  # get '/login' => 'sessions#new'
-  # post '/login' => 'sessions#create'
-  # get '/logout' => 'sessions#destroy'
-
   resources :sessions, only: [:new, :create, :destroy]
-
-  # get '/signup' => 'users#new'
-  # post '/users' => 'users#create'
-
   resources :users, only: [:new, :create]
 
   namespace :admin do
