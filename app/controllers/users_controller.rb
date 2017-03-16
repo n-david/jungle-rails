83add914fd4f86a899884ac41bf29df35e13e3a2
n-david <ngdavid.93@gmail.com>
2017-03-16 02:39:07 +0000

    Implemented order details after placing order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/', notice: 'Success'
+      redirect_to '/'
     else
       render '/signup'
     end
