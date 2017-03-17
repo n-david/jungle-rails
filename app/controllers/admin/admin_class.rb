ee638376569be2182f58926c840e878f295deb4c
n-david <ngdavid.93@gmail.com>
2017-03-17 00:51:42 +0000

    Implemented basic user authentication for admin

---
+++ app/controllers/admin/admin_class.rb
@@ -0,0 +1,3 @@
+# class AdminClass
+#   http_basic_authenticate_with name: ENV["HTTP_AUTH_NAME"], password: ENV["HTTP_AUTH_PASSWORD"]
+# end
--- app/mailers/receipt_mailer.rb
+++ app/mailers/receipt_mailer.rb
@@ -1,8 +1,9 @@
 class ReceiptMailer < ApplicationMailer
-  default from: "lighthouse.labs@yandex.com"
+  default from: "no-reply@jungle.com"
 
   def order_email(order)
     @order = order
+    @line_items = LineItem.where(order_id: @order.id)
     mail(to: @order.email, subject: "Your order from the Jungle (Order ##{@order.id})")
   end
 
